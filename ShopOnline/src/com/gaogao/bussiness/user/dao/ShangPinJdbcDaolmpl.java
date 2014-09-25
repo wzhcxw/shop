d643f5eb4680cfb378c1c69148cf436fa3302e07
johan <weizhaohuan@jd.com>
2014-09-24 23:14:07 -0700

    完善添加文章ui
    添加商品类

---
+++ ShopOnline/src/com/gaogao/bussiness/user/dao/ShangPinJdbcDaolmpl.java
@@ -0,0 +1,15 @@
+package com.gaogao.bussiness.user.dao;
+
+import com.gaogao.bussiness.user.bo.ShangPin;
+import com.gaogao.common.base.JdbcDao;
+
+public class ShangPinJdbcDaolmpl extends  JdbcDao implements ShangPinDao{
+	
+	
+	public boolean addShangPin(ShangPin shangping)
+	{
+
+		return true;
+	}
+
+}
