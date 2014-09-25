f30ec8de29a99852eb8390b438325b4a5b62e45b
johan <weizhaohuan@jd.com>
2014-09-25 00:07:54 -0700

    添加商品接口
    添加商品接口

---
+++ ShopOnline/src/com/gaogao/bussiness/user/service/ShangPinService.java
@@ -0,0 +1,9 @@
+package com.gaogao.bussiness.user.service;
+
+import com.gaogao.bussiness.user.bo.ShangPin;
+
+public interface ShangPinService {
+	
+	public boolean addShangPin(ShangPin shangping);
+
+}
